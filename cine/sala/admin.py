9d0826a80d947db2b2852c6dad44a789ecdcbe68
gustavcaves <rungustavo@gmail.com>
2021-02-11 23:54:14 +0100

    updating

--- cine/sala/admin.py
+++ cine/sala/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Peliculas, programacionSala
+from .models import Peliculas, programacionSala, Sala
 
 # Register your models here.
 
@@ -11,5 +11,10 @@ class programacionSalaAdmin(admin.ModelAdmin):
     list_display = ('datee', 'hour', 'movie')
     readonly_fields = ('created', 'updated')
 
+class SalaAdmin(admin.ModelAdmin):
+    list_display = ('name', 'number_chair')
+    readonly_fields = ('created', 'updated')
+
 admin.site.register(Peliculas, PeliculasAdmin)
 admin.site.register(programacionSala, programacionSalaAdmin)
+admin.site.register(Sala, SalaAdmin)
--- cine/sala/models.py
+++ cine/sala/models.py
@@ -28,3 +28,14 @@ class programacionSala(models.Model):
         verbose_name="Programacion de Película"
         verbose_name_plural="Programación de Películas"
         ordering = ['datee', 'movie']
+
+class Sala(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Nombre de la sala")
+    number_chair = models.IntegerField(verbose_name="Número de Sillas")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación en db")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de última actualización en db")
+    
+    class Meta:
+        verbose_name="Sala"
+        verbose_name_plural="Salas"
+        ordering = ['name', 'number_chair']
